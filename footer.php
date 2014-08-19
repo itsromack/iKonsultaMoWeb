07a037f8941b0e725b721b256f01bf4ed81c4e64
Romack Natividad <romacknatividad@gmail.com>
2014-08-20 02:16:10 +0800

    added tagline to html

--- footer.php
+++ footer.php
@@ -1,27 +1,7 @@
-
-            <div class="l-box-lrg pure-u-1 pure-u-md-3-5">
-                <h4>Contact Us</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat.
-                </p>
-
-                <h4>More Information</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="footer l-box is-center">
-        View the source of this layout to learn more. Made with love by the YUI Team.
-    </div>
-
 </div>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/taglines.js"></script>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -14,10 +14,8 @@
 
 <div class="splash-container">
     <div class="splash">
-        <h1 class="splash-head"><font color="red">i</font><font color="blue">Konsulta</font><font color="yellow">Mo</font></h1>
-        <p class="splash-subhead">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        </p>
+        <h1 class="splash-head"></h1>
+        <p class="splash-subhead"></p>
         <p>
             <a href="http://purecss.io" class="pure-button pure-button-primary">Get Started</a>
         </p>
@@ -108,5 +106,29 @@
                     </fieldset>
                 </form>
             </div>
+
+
+            <div class="l-box-lrg pure-u-1 pure-u-md-3-5">
+                <h4>Contact Us</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+
+                <h4>More Information</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer l-box is-center">
+        View the source of this layout to learn more. Made with love by the YUI Team.
+    </div>
 
 <?php include "footer.php";?>
